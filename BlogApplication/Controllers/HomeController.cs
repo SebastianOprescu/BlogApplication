d10f08ef26a22cc638cd6b3a80431a8522260e21
Me <Me@DESKTOP-G6T2S0A>
2023-03-03 15:44:07 +0200

    Add project files.

---
+++ BlogApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using BlogApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            BlogDatabaseEntities entities = new BlogDatabaseEntities();
+            return View(entities.Blogs.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
